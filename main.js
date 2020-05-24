03991526dd98f34f35d097f740b13e02468d5563
Jhon Neira <40069105+Loda00@users.noreply.github.com>
2020-05-24 18:26:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,48 @@ nodemailer.createTestAccount((err, account) => {
 
 // https://myaccount.google.com/lesssecureapps?pli=1
 // npm i nodemailer --save
+
+
+
+const nodemailer = require('nodemailer')
+const moment = require('moment')
+
+const mailer = async (email, nombre) => {
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL,
+      pass: process.env.PASSWORD_GMAIL
+    }
+  });
+
+  var mailOptions = {
+    from: process.env.GMAIL,
+    to: email,
+    subject: `MIT `,
+    html:`<h1>Correo de confirmación de cuenta</h1>
+          <h3>Se registró correctamente, puede empezar a usar su cuenta.</h3>
+          <h5>Nombre: ${nombre}</h5>
+          <h5>Correo: ${email}</h5>
+          <h5>Fecha: ${moment().format('DD-MM-YYYY')}</h5>
+          <h5>Hora: ${moment().format('HH:mm:ss')}</h5>
+          </br>
+          <strong>${12345}</strong>
+          </br>
+          <h5>Location: PE</h5>
+          <p> * Derechos reservados.</p>
+          </br>
+          </br>
+          <h4>Muchas gracias</h4>
+          <h4>Copyright</h4>`
+  };
+  try {
+    const result = await transporter.sendMail(mailOptions)
+    console.log('result', result)
+    return result
+  } catch (error) {
+    return 500
+  }
+
+}
